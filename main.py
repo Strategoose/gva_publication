b8b1610a00fd78b5acdad982ac78bab6dc5827a9
Max U <maxunsted@gmail.com>
2018-08-07 17:15:42 +0100

    backup

--- main.py
+++ main.py
@@ -81,3 +81,94 @@ temp = temp.loc[temp['sic2'] != 'year_total']
 temp['sic2'] = temp['sic2'].astype(float)
 gva_sectors = pd.merge(gva_sectors, temp, how='left')
 gva_sectors['gva'] = gva_sectors['perc_split'] * gva_sectors['gva_2digit']
+
+combined_gva = gva_sectors
+
+gva_by_sector = combined_gva[['year', 'sector', 'gva']].groupby(['year', 'sector']).sum().reset_index()
+
+
+
+  gva_by_sector <- combined_gva %>%
+
+    # initial summary by sector
+    dplyr::group_by(year, sector) %>%
+    dplyr::summarise(gva = sum(gva)) %>%
+
+    #append total UK GVA by year
+    dplyr::bind_rows(
+      dplyr::filter(gva, sic == "year_total") %>%
+        dplyr::mutate(sector = "UK") %>%
+        dplyr::select(year, sector, gva = gva_2digit)
+    ) %>%
+
+    #append tourism data - add in by statement for transparency
+    dplyr::bind_rows(
+      dplyr::mutate(tourism, sector = "tourism") %>%
+        dplyr::select(year, sector, gva)
+    ) %>%
+
+    #append charitites data
+    dplyr::bind_rows(
+      dplyr::mutate(charities, sector = "charities") %>%
+        dplyr::select(year, sector, gva)
+    )
+
+  #add overlap info from tourism in order to calculate GVA for sector=all_dcms
+  tourism_all_sectors <- dplyr::mutate(tourism, sector = "all_dcms") %>%
+    dplyr::select(year, sector, overlap)
+
+  #add overlap info from tourism in order to calculate GVA for sector=all_dcms
+  charities_all_sectors <- dplyr::mutate(charities, sector = "all_dcms") %>%
+    dplyr::select(year, sector, overlap)
+
+
+  gva_by_sector <-
+    dplyr::left_join(gva_by_sector, tourism_all_sectors, by = c("year", "sector")) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(gva = ifelse(!is.na(overlap), overlap + gva, gva)) %>%
+    dplyr::select(-overlap)
+
+  gva_by_sector <-
+    dplyr::left_join(gva_by_sector, charities_all_sectors, by = c("year", "sector")) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(gva = ifelse(!is.na(overlap), overlap + gva, gva)) %>%
+    dplyr::select(-overlap)
+
+  #final clean up
+  gva_by_sector <- gva_by_sector %>%
+    dplyr::filter(year %in% 2010:max(attr(combined_gva, "years"))) %>%
+    dplyr::select(sector, year, gva) %>%
+    dplyr::arrange(year, sector) %>%
+    data.frame()
+
+
+  sectors_set <- c(
+    "charities"   = "Civil Society (Non-market charities)",
+    "creative"    = "Creative Industries",
+    "culture"     = "Cultural Sector",
+    "digital"     = "Digital Sector",
+    "gambling"    = "Gambling",
+    "sport"       = "Sport",
+    "telecoms"    = "Telecoms",
+    "tourism"     = "Tourism",
+    "all_dcms"    = "All DCMS sectors",
+    "perc_of_UK"  = "% of UK GVA",
+    "UK"          = "UK"
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
